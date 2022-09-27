fc80f469640d4edd820ebcd30ae70c0f54cd68ce
Bogatchuk Roman <bogatchukroma2013@gmail.com>
2022-09-27 15:00:35 +0300

    Initial Commit

---
+++ My Places/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  My Places
+//
+//  Created by Roma Bogatchuk on 27.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
